a455077725ff29f5730dfee40856e76da2b31b55
mathildazanderNTI2C <150040914+mathildazanderNTI2C@users.noreply.github.com>
2025-01-28 16:28:52 +0100

    lektion 28 januari
    kämpar på

--- app.rb
+++ app.rb
@@ -3,3 +3,18 @@ require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
 require 'becrypt'
+
+enable :sessions
+
+def connect_to_db()
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
+    return db
+end
+
+get("/") do 
+    slim(:register)
+end
+
+
+
