f048cb8946a94306c7b7d56da94429e2f15537a0
mathildazanderNTI2C <150040914+mathildazanderNTI2C@users.noreply.github.com>
2025-02-04 16:29:49 +0100

    4 februari

--- app.rb
+++ app.rb
@@ -2,19 +2,33 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
-require 'becrypt'
+#1. 
+# - Kontrollera gems (sinatra, slim, sqlite). Kommer du behöva sessions? Troligen ej, ska endast utföra CRUD på databasen.
+# - Se hur Slimfiler är organierade i mappstrukturen. Följer det REST? Hur kallar man på en slimfil i en mapp?
 
-enable :sessions
+#2. Starta upp applikationen och inspektera koden i Chrome (högerklick>inspect). Hur ser länkarna ut? Finns de som routes i app.rb?
 
-def connect_to_db()
-    db = SQLite3::Database.new('db/database.db')
-    db.results_as_hash = true
-    return db
+#3. När vi klickar på ett album vill vi även se artisten (inte bara id). Gör ett andra anrop till db och skicka med i locals.
+
+#4. Skapa en sida där vi lägger till nya album för tex Artisten ACDC (ArtistId 1). Hitta gärna på nya namn på skivorna
+
+#5. Skapa funktionalitet för att ta bort album
+
+#6. Skapa funktionalitet för att uppdatera artistinformation
+
+
+get('/')  do
+  slim(:index)
 end
 
-get("/") do 
-    slim(:register)
+get('/profilsida') do
+    slim(:profilsida)
 end
 
+get('/login') do 
+    slim(:login)
+end
 
-
+get('/discover') do 
+    slim(:discover)
+end
